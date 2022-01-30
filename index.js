b46f0a6b5a0b8a36eca2df264e3e890eef703a31
PriyanshiChauhan30 <64363969+PriyanshiChauhan30@users.noreply.github.com>
2022-01-30 15:34:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App VSF!')
+  res.send('CICD App VQF!')
 })
 
 app.get('/status', (req, res) => {
